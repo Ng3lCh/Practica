be8f84866a3c0e7803074fb1b25f483017343005
angel <canulchaygrabielangel@gmail.com>
2026-08-09 20:04:35 -0600

    v1.5

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "cambia_esto_por_una_clave_secreta_larga")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "clave_secreta")
 
-    DB_USER = "root"
-    DB_PASSWORD = ""
-    DB_HOST = "localhost"
-    DB_NAME = "oneparfum_db"
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT", "14345")
+    DB_NAME = os.environ.get("DB_NAME", "defaultdb")
 
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
